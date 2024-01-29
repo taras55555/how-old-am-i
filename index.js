75db11d42f7e6c7ef2cf0a5495ce796775e13475
Тарас <tkaminskyi@gmail.com>
2024-01-29 13:56:29 +0200

    - add function to convert seconds to hours and minutes - add new styles

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 dateOfBirthday.addEventListener('input', () => {
     const currentDate = new Date();
+    // currentDate.setHours(0,0,0,0);
     const currentDateYear = currentDate.getFullYear();
 
     const dateBirthday = new Date(dateOfBirthday.value);
+    dateBirthday.setHours(0,0,0,0)
+    console.log(dateBirthday)
     const birthdayDateYear = dateBirthday.getFullYear();
     const birthdayCurrentYear = new Date(dateBirthday);
     birthdayCurrentYear.setFullYear(currentDateYear);
@@ -10,21 +13,43 @@ dateOfBirthday.addEventListener('input', () => {
     let age, birthdayIn, birthdayAfter;
     if (birthdayCurrentYear > currentDate) {
         age = currentDateYear - birthdayDateYear - 1;
-        birthdayIn = (birthdayCurrentYear - currentDate) / 1000 / 86400;
+
+        birthdayIn = daysHoursFromSeconds((birthdayCurrentYear - currentDate) / 1000);
         birthdayCurrentYear.setFullYear(currentDateYear - 1);
-        birthdayAfter = (currentDate - birthdayCurrentYear) / 1000 / 86400;
+        birthdayAfter = daysHoursFromSeconds((currentDate - birthdayCurrentYear) / 1000);
     } else {
         age = currentDateYear - birthdayDateYear;
-        birthdayAfter = (currentDate - birthdayCurrentYear) / 1000 / 86400;
+        birthdayAfter = daysHoursFromSeconds((currentDate - birthdayCurrentYear) / 1000);
         birthdayCurrentYear.setFullYear(currentDateYear + 1);
-        birthdayIn = (birthdayCurrentYear - currentDate) / 1000 / 86400;
-
+        birthdayIn = daysHoursFromSeconds((birthdayCurrentYear - currentDate) / 1000);
     }
-    console.log('age =', age);
-    console.log('birthdayIn =', birthdayIn);
-    console.log('birthdayAfter =', birthdayAfter);
 
-    console.log('========================================');
-    const text = age + '<br>' + birthdayIn + '<br>' + birthdayAfter;
-    myAgeInfo.innerHTML = text;
+    myAgeInfo.textContent = age + ' років';
+    myBirthdayAfter.textContent = birthdayAfter[0] + ' днів ' + pluralizeTime(birthdayAfter[1], 'годин') + ' ' + pluralizeTime(birthdayAfter[2], 'хвилин');
+    myBirthdayIn.textContent = birthdayIn[0] + ' днів ' + pluralizeTime(birthdayIn[1], 'годин') + ' ' + pluralizeTime(birthdayIn[2], 'хвилин');
 });
+
+const daysHoursFromSeconds = (seconds) => {
+    const remainderHours = seconds % 86400;
+    const reminderMinutes = seconds % 3600;
+    const hours = Math.floor(remainderHours / 3600);
+    const minutes = Math.floor(reminderMinutes / 60);
+    const days = Math.floor(seconds / 86400);
+    return [days, hours, minutes];
+}
+
+const pluralizeTime = (digit, timeTemplate) => {
+    return `${digit} ${timeTemplate}${digit >= 5 && digit <= 20
+        ? ''
+        : lastDigit(digit) >= 2 && lastDigit(digit) <= 4
+            ? 'и'
+            : lastDigit(digit) === 1
+                ? 'a'
+                : ''
+        } `;
+}
+
+function lastDigit(inputDigit) {
+    if (inputDigit >= 10) return inputDigit % 10;
+    return inputDigit;
+}
